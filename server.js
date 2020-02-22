8a312f6f91940dbeeb78b7525cfe481061492111
Devon Gadarowski <devon.gadarowski@knights.ucf.edu>
2020-02-22 09:44:56 -0500

    Initial Commit
    Changes to be committed:
    	modified:   .gitignore
    	new file:   frontend/.gitignore
    	new file:   frontend/package.json
    	new file:   frontend/public/favicon.ico
    	new file:   frontend/public/index.html
    	new file:   frontend/public/manifest.json
    	new file:   frontend/public/robots.txt
    	new file:   frontend/src/App.css
    	new file:   frontend/src/App.js
    	new file:   frontend/src/index.js
    	new file:   frontend/src/serviceWorker.js
    	new file:   frontend/src/setupTests.js
    	new file:   package.json
    	new file:   server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+// These make request bodies and cookies easier to read
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Set some HTTP Request Headers (I don't know why)
+app.use((req, res, next) => 
+{
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+	next();
+});
+
+// Frontend Routes -> If someone pings the express server looking
+//                    for the frontend pages, this will forward
+//                    the frontend pages to the requester
+app.use(express.static('./frontend/build'));
+app.get('*', function(req, res)
+{
+	res.sendFile('./frontend/build/index.html', {root: __dirname});
+});
+
+// Start the server listening on port 5000
+app.listen(5000, () => console.log('Express: Server Started'));
