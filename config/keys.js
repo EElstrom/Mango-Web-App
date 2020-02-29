ac4f13dfb1036ae51604b5afab412797ee0b40f2
Justin Miranda <joemface@yahoo.com>
2020-02-29 12:31:39 -0500

    created key.js

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://devong:Shuckle@contactmanager-c6p1t.mongodb.net/contact_manager',
+  cleverKey: 'CC8sn1crKoZ2OM_GyrgyW71r-kg',
+  secretOrKey: 'thesecretisshucklebecauseitprovidesthegreatestdefense'
+};
