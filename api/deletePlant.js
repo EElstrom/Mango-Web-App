53b80702f189d6961d0d72dd08daa97fbf8b9cb8
joemface <joemface@yahoo.com>
2020-03-01 15:17:36 -0500

    accepted changes

--- api/deletePlant.js
+++ api/deletePlant.js
@@ -1,62 +1,5 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-const jwt = require('jsonwebtoken');
-const validator = require('validator');
-const isEmpty = require('is-empty');
-
-const keys = require('../config/keys');
-
-const Plant = require('../models/plant');
-
-function validateInput(data)
-{
-	var errors = {};
-
-	if (isEmpty(data.id) || validator.isEmpty(data.id))
-		errors.id = 'missing id';
-
-	return {errors, isValid: isEmpty(errors)};
-};
-
-router.post('/api/deletePlant', function(req, res, next)
-{
-	console.log('Express: POST /api/deletePlant');
-
-	const authToken = req.cookies.session;
-
-	jwt.verify(authToken, keys.secretOrKey, function(err, user)
-	{
-		if (err || !user)
-		{
-			res.status(401).json({success: false, errors: 'access denied: please login'});
-		}
-		else
-		{
-			const validation = validateInput(req.body);
-
-			if (validation.isValid)
-			{
-				Plant.deleteOne({_id: req.body.id, userId: user.id}, function(err)
-				{
-					if (err)
-					{
-						console.log(err);
-						res.status(500).json({success: false, errors: 'failed to delete plant'});
-					}
-					else
-					{
-						res.status(200).json({success: true});
-					}
-				});
-			}
-			else
-			{
-				res.status(400).json({success: false, errors: validation.errors})
-			}
-		}
-	});
-=======
 const validator = require('validator');
 const isEmpty = require('is-empty');
 
@@ -117,7 +60,6 @@ router.post('./api/deletePlant', (req, res) => {
             errors: validation.errors
         });
     }
->>>>>>> 8acbe7b289013b574d3f7754124490ee122d187a
 });
 
 module.exports = router;
