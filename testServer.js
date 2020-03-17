bd1e1744e2ddbbd59ffa70a496ea1c5cc2ebbe0f
joemface <joemface@yahoo.com>
2020-03-16 22:41:12 -0400

    new npm install

--- testServer.js
+++ testServer.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // establish port; in development: localhost:5000
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // route specifications
 // const addDevice = require('./api/addDevice');
