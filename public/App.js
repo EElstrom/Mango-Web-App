ed1ce0f57bd5f55d1b17937b21d5065f5b38a8da
Justin Miranda <joemface@yahoo.com>
2020-03-18 16:57:18 -0400

    Create App.js

---
+++ public/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import MangoIcon from './components/MangoIcon';
+
+function App()
+{
+	return (
+		<div style={{position: 'fixed', width: '100vw', height: '100vh'}}>
+			<MangoIcon />
+		</div>
+	);
+}
+
+export default App;
