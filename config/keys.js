5767a3058aa5c1d6044a4d348590e27826c57133
Justin Miranda <joemface@yahoo.com>
2020-03-21 20:31:30 -0400

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://eelstrom:treapbestdatastructure@contactmanager-c6p1t.mongodb.net/mango_greenhouse?retryWrites=true&w=majority',
-  secretOrKey: 'thesecretisnolongershucklebecauseisaidsoItsActuallyCharizard'
-};
- 
