945d87c966aba182abeef38ffaecb0f92478f21c
devon-gadarowski <devon.gadarowski@knights.ucf.edu>
2020-03-24 17:15:43 -0400

    React Bus Insurance Meeting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
+// App.css is included in all components, because this file is the root of the project
+import './App.css'
+
+// Import our components so they can be used in the application
+import Title from './components/Title';
+import Login from './components/Login';
+
 // This component is the 'int main()' of our web app: the main entry point
 function App(props)
 {
@@ -11,6 +18,14 @@ function App(props)
 
 				<Route path='/test' exact>
 					<div>Hello World</div>
+				</Route>
+
+				<Route path='/title' exact>
+					<Title text='mango' icon='/mango.png'/>
+				</Route>
+
+				<Route path='/login' exact>
+					<Login />
 				</Route>
 
 				<Redirect to='/test' />
