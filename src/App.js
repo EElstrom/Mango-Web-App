19d0fb9fea9b3915a05954da6b71a42841e0f00a
brooke <brookeshayb@gmail.com>
2020-03-26 16:17:22 -0400

    add login page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 
 import MangoIcon from './components/MangoIcon';
+import Login from './pages/Login';
 
 function App()
 {
 	return (
-		<div style={{position: 'fixed', width: '100vw', height: '100vh'}}>
-			<MangoIcon />
-		</div>
+		<Router>
+			<Switch>
+				<Route style={{position: 'fixed', width: '100vw', height: '100vh'}} path="/" exact>
+					<MangoIcon />
+				</Route>
+				<Route path="/login" exact>
+					<Login />
+				</Route>
+				<Redirect to="/" />
+			</Switch>
+		</Router>
 	);
 }
 
