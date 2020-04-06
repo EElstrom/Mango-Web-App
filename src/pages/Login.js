345570511a53a2ea7228966bae1dc7ddac95e500
brooke <brookeshayb@gmail.com>
2020-04-06 17:34:18 -0400

    Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import '../App.css';
 
 class Login extends React.Component
 {
 	render()
 	{
 		return(
-            <div>hello!</div>
+            <div>
+				<link rel='stylesheet' href="https://fonts.googleapis.com/css?family=Nunito"/>
+				<div class='mango'>mango</div>
+				<img src='/mango2.png' alt='Mango'/>
+			</div>
 		);
 	}
 }
