8a11eddf7673f7e673f0c47940d02ed091ed7cc0
E. Elstrom <47202731+EElstrom@users.noreply.github.com>
2020-04-20 14:38:11 -0400

    secret deleteUser working

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ const getPlants = require('./api/getPlants');
 const deviceLogin = require('./api/deviceLogin');
 const logCondition = require('./api/logCondition');
 const logConditionStupid = require('./api/logConditionStupid');
+const deleteUser = require('./api/deleteUser');
 
 // Set some HTTP Request Headers (I don't know why)
 app.use((req, res, next) => 
@@ -75,6 +76,7 @@ app.use(logout);
 app.use(getUser);
 app.use(editUser)
 app.use(verify);
+app.use(deleteUser);
 
 // Device routes:
 app.use(addDevice);
