b68418909e8d1986c45c7bfb996e3b5124b124f7
brooke <brookeshayb@gmail.com>
2020-04-20 17:18:42 -0400

    CreateGH pages done

--- src/components/CreateGH.js
+++ src/components/CreateGH.js
@@ -8,6 +8,41 @@ const greenhouse_style = {
 	margin: '0px 0px 50px 0px'
 }
 
+const login_field = {
+	color: '#FFFFFF',
+	backgroundColor: '#CDDFBC',
+	outline: 'none',
+	width: '15em',
+	height: '2em',
+  	margin: '15px',
+	borderRadius: '10px',
+
+	borderColor: 'transparent',
+	paddingLeft: '20px',
+	maxWidth: '70%',
+	margin: '10px',
+
+	fontSize: '35px',
+	fontFamily: 'Zilla Slab'
+}
+
+const login_button = {
+	color: '#FFFFFF',
+	backgroundColor: '#75A544',
+	outline: 'none',
+	width: '10em',
+	height: '2em',
+	borderRadius: '10px',
+
+	borderColor: 'transparent',
+	maxWidth: '70%',
+	margin: '15px 10px 20px 10px',
+
+
+	fontSize: '35px',
+	fontFamily: 'Zilla Slab'
+}
+
 // card prompting creation of a gh
 class Create_gh_prompt extends React.Component
 {
@@ -25,32 +60,74 @@ class Create_gh_prompt extends React.Component
 // card to create a new gh
 class Create_gh extends React.Component
 {
+	constructor(props)
+    {
+        super(props);
+
+        this.state = {
+			ghName: '',
+			plantName: '',
+			plantSpecies: ''
+        };
+	}
+
+	// createDevice with inputted name
 	createGH = async event =>
     {
         event.preventDefault();
 
-        // TODO Make API Call Here (See API Specs on GitHub Wiki)
+		// TODO Make API Call Here (See API Specs on GitHub Wiki)
+		
+		this.setState({});
+	}
 
-        // this.setState({message: this.state.email.value + ' ' + this.state.password.value});
+	// add plant to greenhouse
+	addPlant = async event =>
+	{
+		event.preventDefault();
+
+		// TODO Make API Call Here (See API Specs on GitHub Wiki)
+		
+		this.setState({});
 	}
 	
 	render()
 	{
 		return(
-			<div>
-                <div class='card'>
-					<form onSubmit={this.createGH}>
-						gh create page
-						{/* <link rel='stylesheet' href="https://fonts.googleapis.com/css?family=Zilla+Slab:700"/>
-						<input style={login_field} type='text' placeholder='email' ref={(value) => this.state.email = value}/>
-						<input style={login_field} type='password' placeholder='password' ref={(value) => this.state.password = value}/>
-						<Link to="/reset-password" style={underline_link_space}>forgot password?</Link>
-						<input style={login_button} type='submit' value='Log In'/> */}
-					</form>
-                </div>
+			<div class='card'>
+				{(this.state.ghName === '') ?
+				<div>
+				<form onSubmit={this.createGH}>
+					<link rel='stylesheet' href="https://fonts.googleapis.com/css?family=Nunito:900"/>
+					<link rel='stylesheet' href="https://fonts.googleapis.com/css?family=Zilla+Slab:700"/>
+					<div class='mango'>name your greenhouse:</div>
+					{/* <input style={login_field} type='text' id='ghName' placeholder='My greenhouse' ref={(value) => this.state.ghName = value}/><br /> */}
+					<input style={login_button} type='submit' id='createGH' value='Create'/>
+				</form>
+				</div>
+				:
+				<div>
+					<form onSubmit={this.addPlant}>
+					<link rel='stylesheet' href="https://fonts.googleapis.com/css?family=Nunito:900"/>
+					<link rel='stylesheet' href="https://fonts.googleapis.com/css?family=Zilla+Slab:700"/>
+					<div class='mango'>nice! now add your first plant:</div>
+					<input style={login_field} type='text' id='plantName' placeholder='Species (e.g. Geranium)' ref={(value) => this.state.plantName = value}/><br />
+					<input style={login_field} type='text' id='plantType' placeholder='Name (e.g. geranium #2)' ref={(value) => this.state.plantSpecies = value}/><br />
+					<input style={login_button} type='submit' id='addPlant' value='Add to greenhouse' />
+				</form>
+				</div>}
 			</div>
 		);
 	}
 }
+
+// name : req.body.name,
+// notes : pnotes,
+// deviceName : dName,
+// type : pType,
+// temperatureTolerance : tempTol,
+// lightTolerance : lightTol,
+// phTolerance : phTol,
+// humidityTolerance : humTol
 
 export {Create_gh_prompt, Create_gh};
