80885c7900909153fd6ee8c57e34658b7a2fb71c
brooke <brookeshayb@gmail.com>
2020-04-21 17:15:48 -0400

    Settings editing

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -23,7 +23,7 @@ const name_style = {
     color: '#000000',
       
     position: 'fixed',
-    top: '35%',
+    top: '30%',
     left: '15%',
 }
 
@@ -34,7 +34,7 @@ const email_style = {
     color: '#000000',
       
     position: 'fixed',
-    top: '50%',
+    top: '45%',
     left: '15%',
 }
 
@@ -45,7 +45,7 @@ const password_style = {
     color: '#000000',
       
     position: 'fixed',
-    top: '65%',
+    top: '60%',
     left: '15%',
 }
 
@@ -56,21 +56,154 @@ const pollFreq_style = {
     color: '#000000',
       
     position: 'fixed',
-    top: '80%',
+    top: '75%',
     left: '15%',
+}
+
+const login_field = {
+	color: '#FFFFFF',
+	backgroundColor: '#CDDFBC',
+	outline: 'none',
+	width: '15em',
+	height: '2em',
+  	margin: '15px',
+	borderRadius: '10px',
+
+	borderColor: 'transparent',
+	paddingLeft: '20px',
+	maxWidth: '70%',
+	margin: '10px',
+
+	fontSize: '35px',
+	fontFamily: 'Zilla Slab'
+}
+
+const login_button = {
+	color: '#FFFFFF',
+	backgroundColor: '#75A544',
+	outline: 'none',
+	width: '8em',
+	height: '2em',
+	borderRadius: '10px',
+
+	borderColor: 'transparent',
+	maxWidth: '70%',
+	margin: '15px 10px 20px 10px',
+
+
+	fontSize: '30px',
+	fontFamily: 'Zilla Slab'
+}
+
+const settings_card = {
+    backgroundColor: '#FFFFFF',
+    minHeight: '60%',
+    minWidth: '85%',
+        
+    width: 'auto',
+    height: 'auto',
+        
+    position: 'fixed',
+    top: '25%',
+    left: '4.5%',
+
+    borderRadius: '15px',
+    padding: '39px'
 }
 
 class Settings extends React.Component
 {
+	constructor(props)
+    {
+        super(props);
+
+        this.state = {
+			nameEdit: 'false',
+			emailEdit: 'false',
+			passEdit: 'false',
+			pollEdit: 'false'
+        };
+	}
+
+	submitEdit = async event => {
+		// API call here
+	}
+
+	Edit_form = (ph) => {
+		return (
+			<form onSubmit={this.submitEdit}>
+				<link rel='stylesheet' href="https://fonts.googleapis.com/css?family=Zilla+Slab:700"/>
+				<input style={login_field} type='text' placeholder={ph} ref={(value) => this.state.email = value}/>
+				<input style={login_button} type='submit' value='Save'/>
+			</form>
+		);
+	}
+
+	startNameEdit = async event => {
+		event.preventDefault();
+		this.setState({nameEdit: 'true'})
+	}
+
+	startEmailEdit = async event => {
+		event.preventDefault();
+		this.setState({emailEdit: 'true'})
+	}
+
+	startPassEdit = async event => {
+		event.preventDefault();
+		this.setState({passEdit: 'true'})
+	}
+
+	startPollEdit = async event => {
+		event.preventDefault();
+		this.setState({pollEdit: 'true'})
+	}
+
 	render()
 	{
 		return(
 			<div style={yellow}>
-				<div class='card'>
-					<div style={name_style}>name: </div>
-					<div style={email_style}>email: </div>
-					<div style={password_style}>password: </div>
-					<div style={pollFreq_style}>polling frequency: </div>
+				<div style={settings_card}>
+					<div>
+						{(this.state.nameEdit === 'false') ? 
+						<div style={name_style}>
+							name: name here
+							<form onSubmit={this.startNameEdit}>
+								<link rel='stylesheet' href="https://fonts.googleapis.com/css?family=Zilla+Slab:700"/>
+								<input style={login_button} type='submit' value='Edit'/>
+							</form>
+						</div> : this.Edit_form('new name')}
+					</div>
+					<div>
+						{(this.state.emailEdit === 'false') ? 
+						<div style={email_style}>
+							email: email here
+							<form onSubmit={this.startEmailEdit}>
+								<link rel='stylesheet' href="https://fonts.googleapis.com/css?family=Zilla+Slab:700"/>
+								<input style={login_button} type='submit' value='Edit'/>
+							</form>
+						</div> : this.Edit_form('new email')}
+					</div>
+					<div>
+						{(this.state.passEdit === 'false') ? 
+						<div style={password_style}>
+							password: password here
+							<form onSubmit={this.startPassEdit}>
+								<link rel='stylesheet' href="https://fonts.googleapis.com/css?family=Zilla+Slab:700"/>
+								<input style={login_button} type='submit' value='Edit'/>
+							</form>
+						</div> : this.Edit_form('new password')}
+					</div>
+					<div>
+						{(this.state.pollEdit === 'false') ? 
+						<div style={pollFreq_style}>
+							polling frequency: polling frequency here
+							<form onSubmit={this.startPollEdit}>
+								<link rel='stylesheet' href="https://fonts.googleapis.com/css?family=Zilla+Slab:700"/>
+								<input style={login_button} type='submit' value='Edit'/>
+							</form>
+						</div> : this.Edit_form('new polling frequency')}
+					</div>
 				</div>
 			</div>
 		);
